c28622947243d41251fbd264d1492f4e7d7a49c9
Dieter Vandenbussche <dvandenbussche@axioma.com>
2013-07-06 14:52:28 -0400

    Try to mock pandas for the purpose of building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,28 @@ import os.path
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(os.path.join('..','..','xlsxplt_pandas')))
 
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pandas']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
